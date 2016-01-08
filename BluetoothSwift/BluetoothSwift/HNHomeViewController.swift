adeb3a0f84d30e1ec74687edbbfb3ac3c7b13617
Antony.Wong <aeq2005@163.com>
2016-01-08 20:28:45 +0800

    链接HOME页面的各种控件。并实现委托方法。

--- BluetoothSwift/BluetoothSwift/HNHomeViewController.swift
+++ BluetoothSwift/BluetoothSwift/HNHomeViewController.swift
@@ -8,28 +8,42 @@
 
 import UIKit
 
-class HNHomeViewController: UIViewController {
+class HNHomeViewController: UIViewController, HNCustomLightButtonDelegate, SwiftHSVColorPickerDelegate {
 
+    
+    /// 色盘
+    @IBOutlet weak var colorPicker: SwiftHSVColorPicker!
+    
+    /// 8个灯按钮
+    @IBOutlet var lightButtonViews: [UIView]!
+    
+    /// 灯开关按钮
+    @IBOutlet weak var lightToggleButton: UIButton!
+    
+    /// 切换到编辑页面按钮
+    @IBOutlet weak var editPageToggleButton: UIButton!
+    
+    /// 群组编辑按钮
+    @IBOutlet var groupButtons: [UIButton]!
+    
+    /// 亮度调节slider
+    @IBOutlet weak var brightnessSlider: UISlider!
+    
+    // MARK:-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK:- HNCustomLightButtonDelegate
+    func click(view: HNCustomLightButton, tag: Int) {
+        print("调用了HNCustomLightButtonDelegate中的方法")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK:- SwiftHSVColorPickerDelegate
+    func touchColorWheel(view: SwiftHSVColorPicker, color: UIColor, brightness: CGFloat, colorR: CGFloat, colorG: CGFloat, colorB: CGFloat) {
+        print("调用SwiftHSVColorPickerDelegate")
     }
-    */
-
+    
 }
